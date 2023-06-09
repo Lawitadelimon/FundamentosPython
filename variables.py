c5d4a73c06e1cc11acccba2172cdaaeda3c360f6
lawitadelimon <lau72abrvjua@gmail.com>
2023-06-09 16:21:07 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+#Python es No tipado
+
+name = "Laura"
+last_name = "Juarez"
+age = 19
+height = 1.60
+is_student = True
+geoup = ["3C"] 
+
+#Function type
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(group))
