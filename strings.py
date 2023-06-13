f021748ef4ec801c4cc44352109b09a94edc0aaa
lawitadelimon <lau72abrvjua@gmail.com>
2023-06-13 13:01:28 -0600

    07 Strings

---
+++ strings.py
@@ -0,0 +1,25 @@
+my_string = "esTe eS Mi StrInG"
+
+# longitud de string
+print(len(my_string))
+# Mayúsculas
+print(my_string.upper())
+# Minúsculas
+print(my_string.lower())
+# Capitalice primera letra a Mayus
+print(my_string.capitalize())
+# Eliminar espacios inicio fin
+print(my_string.strip())
+
+# Indexing and Slicing
+my_string = "Hola 3C DSM"
+print(my_string [0])
+print(my_string [6])
+print(my_string [3])
+print(my_string [-1])
+print(my_string [-2])
+
+# Dos puntos indican rango
+print(my_string [0:2])
+print(my_string [5:7])
+print(my_string [5:-1])
